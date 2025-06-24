b8eefd8af366ac3ac84644c2f261a58b1e9d07e9
Josh Haliday <josh.haliday@gmail.com>
2025-06-24 10:51:48 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -46,8 +46,8 @@ const AlertTitle = React.forwardRef<
 AlertTitle.displayName = "AlertTitle"
 
 const AlertDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
